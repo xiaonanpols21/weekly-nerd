a0a522c6ae66d2f37c15b463ea16a170bdf8ebb8
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-02-18 13:28:22 +0100

    style > index, kijken h1 gradient, nav klaarmaken voor js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const port = 3000;
 // Zie prompts: https://chemical-bunny-323.notion.site/Weekly-Nerd-Chat-GPT-Documentation-6764544211dc42158c23d85eec350fc4#7a7561db865749019571f89bbc464bca
 function compileSass() {
     sass.render({
-        file: 'public/styles/style.scss',
+        file: 'public/styles/index.scss',
         outputStyle: 'compressed' // Set output style, e.g., compressed, expanded
     }, function(err, result) {
         if (!err) {
-            fs.writeFile('public/styles/style.css', result.css, function(err){
+            fs.writeFile('public/styles/index.css', result.css, function(err){
                 if(!err){
                     console.log('Sass compiled successfully');
                 } else {
