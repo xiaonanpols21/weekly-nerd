d3ba7d40865f2af7431c701bd39f7c53e506905b
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-02-18 15:13:59 +0100

    js functions gemaakt

---
+++ public/js/index.js
@@ -0,0 +1,74 @@
+/*
+    Variables
+*/
+// Nav buttons
+const leftBtn = document.querySelector(".nav-page a:first-of-type");
+const rightBtn = document.querySelector(".nav-page a:last-of-type");
+const nav1 = document.querySelector(".nav-page p:first-of-type");
+
+// Section in screen?
+const heroSec = document.querySelector("#sec-1");
+const blogSec = document.querySelector("#sec-2");
+
+// Hero carousel
+const carousel1 = document.querySelector(".carousel-hero a:first-of-type");
+const carouselArticles = document.querySelectorAll(".carousel-hero a");
+
+/*
+    Functions
+*/
+// Nav buttons
+leftBtn.classList.add("disabled");
+
+function changeNavRight() {
+    nav1.textContent = "2";
+    leftBtn.classList.remove("disabled");
+    rightBtn.classList.add("disabled");
+}
+rightBtn.addEventListener("click", changeNavRight);
+
+function changeNavLeft() {
+    nav1.textContent = "1";
+    leftBtn.classList.add("disabled");
+    rightBtn.classList.remove("disabled");
+}
+leftBtn.addEventListener("click", changeNavLeft);
+
+
+// Sections in screen?
+// Zie prompts: https://chemical-bunny-323.notion.site/Weekly-Nerd-Chat-GPT-Documentation-6764544211dc42158c23d85eec350fc4#79c01870e6d44bfe99da2de35159c2f7
+// FIXME: 
+function isScreenUnder992() {
+    return window.innerWidth < 992;
+}
+
+if (isScreenUnder992()) {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                nav1.textContent = "1";
+                console.log("In screen");
+            } else {
+                nav1.textContent = "2";
+                console.log("Out of screen");
+            }
+        });
+    }, { threshold: 1 });
+
+    observer.observe(heroSec);
+
+}
+
+// Hero carousel
+// Zie prompts: https://chemical-bunny-323.notion.site/Weekly-Nerd-Chat-GPT-Documentation-6764544211dc42158c23d85eec350fc4#501d61b30a2846bdb3f891d521ccc7e0
+carousel1.classList.add("active");
+
+function addActive(event) {
+    document.querySelector(".carousel-hero a.active").classList.remove("active");
+    event.target.classList.add("active");
+}
+
+carouselArticles.forEach(carouselArticle => {
+    carouselArticle.addEventListener("mouseover", addActive);
+});
+
